d775b925b5977126ce19e8bfa5cb0f67e6d48654
p-derezinski <p.derezinski@gmail.com>
2019-06-22 13:40:14 +0200

    Initial commit

---
+++ src/main/java/pl/carrentalsda/carrental/CarrentalApplication.java
@@ -0,0 +1,13 @@
+package pl.carrentalsda.carrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrentalApplication.class, args);
+	}
+
+}
