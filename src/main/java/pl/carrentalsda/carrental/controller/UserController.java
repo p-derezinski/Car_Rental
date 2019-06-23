91bb5d8438127a4e3cfa80615fbf798b18d95904
p-derezinski <p.derezinski@gmail.com>
2019-06-23 11:15:26 +0200

    Added initial GetMapping methods in UserController

--- src/main/java/pl/carrentalsda/carrental/controller/UserController.java
+++ src/main/java/pl/carrentalsda/carrental/controller/UserController.java
@@ -1,4 +1,30 @@
 package pl.carrentalsda.carrental.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+
+    // wejście na stronę rejestracji
+    @GetMapping("/register")
+    public String register(Model model){
+//        model.addAttribute("user", new UserDto());
+        return "register";
+    }
+
+    // wejście na stronę logowania
+    @GetMapping("/login")
+    public String login(Model model){
+//        model.addAttribute("user", new UserDto());
+        return "login";
+    }
+
+    // wejście na stronę kontakt
+    @GetMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
+
 }
