5271c7578c4def25651a9b2a9fad5b4ca2a5af2f
p-derezinski <p.derezinski@gmail.com>
2019-06-25 01:18:19 +0200

    Added the basics of registration functionality
    - temporary change is Users class was necessary in order to make the app work
      (Id not generated automatically)

--- src/main/java/pl/carrentalsda/carrental/controller/UserController.java
+++ src/main/java/pl/carrentalsda/carrental/controller/UserController.java
@@ -1,17 +1,45 @@
 package pl.carrentalsda.carrental.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pl.carrentalsda.carrental.controller.dto.UsersDto;
+import pl.carrentalsda.carrental.service.UsersService;
+
+import javax.validation.Valid;
 
 @Controller
 public class UserController {
 
+    // pole do wstrzykniecia
+    UsersService usersService;
+
+    // wstrzyknięcie zależności przez konstruktor
+    @Autowired
+    public UserController(UsersService usersService) {
+        this.usersService = usersService;
+    }
+
     // wejście na stronę rejestracji
     @GetMapping("/register")
     public String register(Model model){
-//        model.addAttribute("user", new UserDto());
+        model.addAttribute("users", new UsersDto());
         return "register";
+    }
+
+    // obsługa wysłanego formularza
+    @PostMapping("/register")
+    public String register(@ModelAttribute("users") @Valid UsersDto usersDto,
+                           BindingResult bindingResult){
+        if (bindingResult.hasErrors()) {
+            return "register";
+        }
+        usersService.saveUser(usersDto);
+        return "redirect:/";
     }
 
     // wejście na stronę logowania
--- src/main/java/pl/carrentalsda/carrental/controller/dto/UsersDto.java
+++ src/main/java/pl/carrentalsda/carrental/controller/dto/UsersDto.java
@@ -2,9 +2,12 @@ package pl.carrentalsda.carrental.controller.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class UsersDto {
 
+    @NotBlank(message = "Insert your first name")
     private String firstname;
     private String lastname;
     private String email;
@@ -12,5 +15,7 @@ public class UsersDto {
     private String city;
     private String street;
     private String password;
+
+    // TODO - uzupełnić walidację
 
 }
