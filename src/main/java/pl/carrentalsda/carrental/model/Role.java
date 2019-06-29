05b55859594a0f6b5e6c61542f4f0c580aa50d6a
p-derezinski <p.derezinski@gmail.com>
2019-06-29 10:19:55 +0200

    Added Role and RoleRepository, changed saveUser method

---
+++ src/main/java/pl/carrentalsda/carrental/model/Role.java
@@ -0,0 +1,23 @@
+package pl.carrentalsda.carrental.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String role_name;
+
+}
--- src/main/java/pl/carrentalsda/carrental/service/UsersService.java
+++ src/main/java/pl/carrentalsda/carrental/service/UsersService.java
@@ -4,16 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.carrentalsda.carrental.controller.dto.UsersDto;
 import pl.carrentalsda.carrental.model.Users;
+import pl.carrentalsda.carrental.repository.RoleRepository;
 import pl.carrentalsda.carrental.repository.UsersRepository;
 
 @Service
 public class UsersService {
 
     UsersRepository usersRepository;
+    RoleRepository roleRepository;
 
     @Autowired
-    public UsersService(UsersRepository usersRepository) {
+    public UsersService(UsersRepository usersRepository, RoleRepository roleRepository) {
         this.usersRepository = usersRepository;
+        this.roleRepository = roleRepository;
     }
 
     public void saveUser(UsersDto usersDto){
@@ -25,6 +28,8 @@ public class UsersService {
                         usersDto.getCity(),
                         usersDto.getStreet(),
                         usersDto.getPassword());
+        // dodajemy rolę użytkownika
+        user.addRole(roleRepository.getOne(1L));
         // zapis do bazy danych
         System.out.println(user);
         usersRepository.save(user);
